056003363625b7eefd5ca54a690c71c171661e19
Daniel <xpatek08@stud.fit.vutbr.cz>
2020-11-11 20:50:18 +0100

    starting with auth

--- models.py
+++ models.py
@@ -1,13 +1,22 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
+
 from extensions import db
 
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(60))
     password = db.Column(db.String(100))
     isAdmin = db.Column(db.Boolean)
-    isOwner = db.Column(db.Boolean)
-    isManager = db.Column(db.Boolean)
-    isCustomer = db.Column(db.Boolean)
+
+    @property
+    def unhashed_password(self):
+        raise AttributeError('cannot lol')
+
+    @unhashed_password.setter
+    def unhashed_password(self, unhashed_password):
+        self.password = generate_password_hash(unhashed_password)
+
 
 class Hotel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from extensions import db
 from models import Users, Hotel
@@ -8,3 +8,26 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        unhashed_pass = request.form['password']
+        
+        user = Users(
+            name = name, 
+            unhashed_password = unhashed_pass, 
+            isAdmin = True
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
