921ea6e3f56e6e9738ccc5ce9841cfc60db9d339
Daniel Čechák <sonaga98@gmail.com>
2020-11-24 18:52:52 +0100

    update - dashboard

--- appdata/routes/dashboard.py
+++ appdata/routes/dashboard.py
@@ -11,3 +11,39 @@ def dash_index():
         return redirect(url_for('main.index'))
     else:
         return render_template('dashboard_index.html')
+@dash.route('/users', methods=['GET'])
+def users():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_users.html')
+@dash.route('/hotels', methods=['GET'])
+def hotels():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_hotels.html')
+@dash.route('/rooms', methods=['GET'])
+def rooms():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_rooms.html')
+@dash.route('/reservation', methods=['GET'])
+def reservation():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_reservation.html')
+@dash.route('/running', methods=['GET'])
+def running():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_running.html')
+@dash.route('/ended', methods=['GET'])
+def ended():
+    if not current_user.isEmployee:
+         return redirect(url_for('main.index'))
+    else:
+        return render_template('dashboard_ended.html')
